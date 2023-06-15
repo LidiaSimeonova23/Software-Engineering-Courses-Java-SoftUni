d5887f9f45e63821adba4d4621a03707ec06b84b
Lidia Simeonova <lidia_simeonova2023@abv.bg>
2023-06-15 22:45:45 +0300

    Added lecture13DefiningClasses
    Added lecture 13 with the solution of problem 1 from the lecture

---
+++ Course 3.1.  Java Advanced/lecture13DefiningClasses/P01CarInfo/Main.java
@@ -0,0 +1,31 @@
+package lecture13DefiningClasses.P01CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfCars = Integer.parseInt(scanner.nextLine());    //четем от първия ред на конзолата брой обекти в класа (брой коли)
+
+        for (int currentCar = 1; currentCar <= numberOfCars; currentCar++) {
+            String dataOfCurrentCar = scanner.nextLine();     //на всяка итерация четем данните за текущия обект (текущата кола)   // "{brand} {model} {horsePower}"
+            String[] partsOfCurrentCarArray = dataOfCurrentCar.split("\\s+");       //["brand", "model", "horsePower"]
+            String brandOfCurrentCar = partsOfCurrentCarArray[0];
+            String modelOfCurrentCar = partsOfCurrentCarArray[1];
+            int horsePowerOfCurrentCar = Integer.parseInt(partsOfCurrentCarArray[2]);
+
+            //създаваме нов обект (който е празен) в класа Car
+            Car car = new Car();
+
+            //на създадения от нас обект му сетваме данните, които сме прочели от конзолата
+            car.setBrand(brandOfCurrentCar);
+            car.setModel(modelOfCurrentCar);
+            car.setHorsePower(horsePowerOfCurrentCar);
+
+            System.out.println(car.toString());      //отпечатваме обекта под формата на текст
+            //System.out.println(car);
+        }
+
+    }
+}
