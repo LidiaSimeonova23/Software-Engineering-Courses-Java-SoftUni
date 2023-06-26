7148570d873e3b761459e87019dacc093391ca2b
Lidia Simeonova <lidia_simeonova2023@abv.bg>
2023-06-26 23:34:46 +0300

    Added solutions of problem 3 from exam - 28 and 29 March 2020

---
+++ Course 1. Programming Basics/pbExam1Date28And29March2020/P03FitnessCardVersion4.java
@@ -0,0 +1,65 @@
+package pbExam1Date28And29March2020;
+
+import java.util.Scanner;
+
+public class P03FitnessCardVersion4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double availableBudget = Double.parseDouble(scanner.nextLine());         //•	Сумата, с която разполагаме - реално число в интервала [10.00…1000.00]
+        char gender = scanner.nextLine().charAt(0);       //•	Пол - символ ('m' за мъж и 'f' за жена)
+        int age = Integer.parseInt(scanner.nextLine());      //•	Възраст - цяло число в интервала [5…105]
+        String sport = scanner.nextLine();         //•	Спорт - текст (една от възможностите в таблицата: "Gym", "Boxing", "Yoga", "Zumba", "Dances", "Pilates")
+
+        //програма, която проверява дали първоначално налична сума е достатъчна, за да се заплати карта за месечен достъп във фитнес
+        //Цената на картата зависи от пола на клиента и спорта
+
+
+        double priceOfCard = 0;
+        if (gender == 'm') {
+            if (sport.equals("Gym")) {
+                priceOfCard = 42;
+            } else if (sport.equals("Boxing")) {
+                priceOfCard = 41;
+            } else if (sport.equals("Yoga")) {
+                priceOfCard = 45;
+            } else if (sport.equals("Zumba")) {
+                priceOfCard = 34;
+            } else if (sport.equals("Dances")) {
+                priceOfCard = 51;
+            } else if (sport.equals("Pilates")) {
+                priceOfCard = 39;
+            }
+        } else if (gender == 'f') {
+            if (sport.equals("Gym")) {
+                priceOfCard = 35;
+            } else if (sport.equals("Boxing")) {
+                priceOfCard = 37;
+            } else if (sport.equals("Yoga")) {
+                priceOfCard = 42;
+            } else if (sport.equals("Zumba")) {
+                priceOfCard = 31;
+            } else if (sport.equals("Dances")) {
+                priceOfCard = 53;
+            } else if (sport.equals("Pilates")) {
+                priceOfCard = 37;
+            }
+        }
+
+
+        //Всички цени на карти за ученици (възраст под 19 години вкл.) са с 20% намаление.
+        if (age <= 19) {
+            priceOfCard = priceOfCard - (priceOfCard * 20 / 100);
+        }
+
+
+        if (availableBudget >= priceOfCard) {           //•	Ако сумата е достатъчна
+            System.out.printf("You purchased a 1 month pass for %s.", sport);
+        } else {         //•	Ако сумата не е достатъчна
+            double missingMoney = priceOfCard - availableBudget;
+            System.out.printf("You don't have enough money! You need $%.2f more.", missingMoney);
+        }
+
+
+    }
+}
