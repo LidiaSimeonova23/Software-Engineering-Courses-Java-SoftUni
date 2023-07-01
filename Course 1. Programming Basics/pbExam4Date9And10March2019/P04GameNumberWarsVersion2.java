9420c9ee4eda943d673d814668753b0915feb5ff
Lidia Simeonova <lidia_simeonova2023@abv.bg>
2023-07-01 23:38:37 +0300

    Added solutions of problem 4

---
+++ Course 1. Programming Basics/pbExam4Date9And10March2019/P04GameNumberWarsVersion2.java
@@ -0,0 +1,52 @@
+package pbExam4Date9And10March2019;
+
+import java.util.Scanner;
+
+public class P04GameNumberWarsVersion2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //карти:  двойки, тройки, четворки, петици, шестици, седмици, осмици, деветки и десетки от всички 4 бои
+
+        //Първоначално се четат два реда:
+        String nameOfFirstPlayer = scanner.nextLine();          //Име на първия играч - текст
+        String nameOfSecondPlayer = scanner.nextLine();         //Име на втория играч - текст
+
+        //След това, до получаване на команда "End of game", се четат многократно по два реда:
+        int pointsOfFirstPlayer = 0;       //създаваме си променлива за точките на първия играч
+        int pointsOfSecondPlayer = 0;      //създаваме си променлива за точките на втория играч
+        boolean numberWarsHasHappened = false;
+        String input = scanner.nextLine();
+        while (!input.equals("End of game")) {
+            int cardOfFirstPlayer = Integer.parseInt(input);          //Карта, която дава първият играч- цяло число в интервала [2…9]
+            int cardOfSecondPlayer = Integer.parseInt(scanner.nextLine());         //Карта, която дава вторият играч -  цяло число в интервала [2…9]
+
+            if (cardOfFirstPlayer > cardOfSecondPlayer) {       //Ако картата на първия играч е с по-голяма стойност от картата на втория играч, първият получава точки, които са равни на разликата между картата на първия и картата на втория
+                pointsOfFirstPlayer += cardOfFirstPlayer - cardOfSecondPlayer;
+            } else if (cardOfFirstPlayer < cardOfSecondPlayer) {      //Ако картата на втория играч е с по-голяма стойност от картата на първия играч, вторият получава точки, които са равни на разликата между картата на втория и картата на първия
+                pointsOfSecondPlayer += cardOfSecondPlayer - cardOfFirstPlayer;
+            } else {         //else if (cardOfFirstPlayer == cardOfSecondPlayer)
+                numberWarsHasHappened = true;
+                System.out.println("Number wars!");
+                //При еднакви карти на двамата играчи се прочитат нови два реда (карта, която дава първият, и карта, която дава вторият.)
+                int additionalCardOfFirstPlayer = Integer.parseInt(scanner.nextLine());
+                int additionalCardOfSecondPlayer = Integer.parseInt(scanner.nextLine());
+
+                //Играчът, чиято допълнителна карта е с по-голяма стойност, печели и играта приключва
+                if (additionalCardOfFirstPlayer > additionalCardOfSecondPlayer) {
+                    System.out.printf("%s is winner with %d points", nameOfFirstPlayer, pointsOfFirstPlayer);
+                } else {
+                    System.out.printf("%s is winner with %d points", nameOfSecondPlayer, pointsOfSecondPlayer);
+                }
+                return;         //прекъсваме (приключваме) цялата програма
+            }
+
+            input = scanner.nextLine();      //update
+        }
+
+
+        //щом програмата не е била прекъсната (заради еднакви карти), значи сме достигнали до команда "End of game", няма победител, просто си принтираме точките на всеки от играчите:
+        System.out.printf("%s has %d points%n", nameOfFirstPlayer, pointsOfFirstPlayer);
+        System.out.printf("%s has %d points", nameOfSecondPlayer, pointsOfSecondPlayer);
+    }
+}
