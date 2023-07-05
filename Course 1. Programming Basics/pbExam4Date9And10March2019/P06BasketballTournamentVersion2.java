acf5e7de389d577867debe8e2481941f111dd2aa
Lidia Simeonova <lidia_simeonova2023@abv.bg>
2023-07-05 23:06:35 +0300

    Added solutions of the other problem with number 6 from exam - 9 and 10 March 2019

---
+++ Course 1. Programming Basics/pbExam4Date9And10March2019/P06BasketballTournamentVersion2.java
@@ -0,0 +1,51 @@
+package pbExam4Date9And10March2019;
+
+import java.util.Scanner;
+
+public class P06BasketballTournamentVersion2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //От конзолата се четат поредица от турнири до получаване на командата "End of tournaments" (не знаем колко ще са повторенията -> while цикъл):
+        int winsCount = 0;      //създаваме си променлива за броя на победите
+        int lossesCount = 0;      //създаваме си променлива за броя на загубите
+        int totalNumberOfGamesFromAllTournaments = 0;       //създаваме си променлива за общия брой мачове от всички турнири
+
+        String input = scanner.nextLine();
+        while (!input.equals("End of tournaments")) {
+            String nameOfCurrentTournament = input;          //Име на турнира – текст
+            int numberOfGamesOfCurrentTournament = Integer.parseInt(scanner.nextLine());          //За всеки турнир n на брой мача – цяло число в интервала [1…15]
+
+            //намираме общия брой мачове от абсолютно всички турнири
+            totalNumberOfGamesFromAllTournaments += numberOfGamesOfCurrentTournament;        //totalNumberOfGamesFromAllTournaments = totalNumberOfGamesFromAllTournaments + numberOfGamesOfCurrentTournament;
+
+            //За всеки мач (а ние знаем колко са мачовете -> имаме for цикъл) се четат по два реда:
+            for (int currentGame = 1; currentGame <= numberOfGamesOfCurrentTournament; currentGame++) {
+                int numberOfPointsOfTeamOfDesi = Integer.parseInt(scanner.nextLine());         //Точки, вкарани от отбора на Деси – цяло число в интервала от [0…150]
+                int numberOfPointsOfOpposingTeam = Integer.parseInt(scanner.nextLine());       //Точки, вкарани от противниковия отбор – цяло число в интервала от [0…150]
+
+                //Няма да има случаи на равенство. -> няма да имаме проверка за ревенство == на точките на двата отбора
+                if (numberOfPointsOfTeamOfDesi > numberOfPointsOfOpposingTeam) {         //Ако отборът на Деси е спечелил
+                    winsCount++;      //winsCount += 1;        //winsCount = winsCount + 1;     //броят на победите се увеличава с единица
+                    System.out.printf("Game %d of tournament %s: win with %d points.%n", currentGame, nameOfCurrentTournament, numberOfPointsOfTeamOfDesi - numberOfPointsOfOpposingTeam);
+                } else {      //else if (numberOfPointsOfTeamOfDesi < numberOfPointsOfOpposingTeam)       //Ако отборът на Деси е загубил:
+                    lossesCount++;        //броят на загубите се увеличава с единица
+                    System.out.printf("Game %d of tournament %s: lost with %d points.%n", currentGame, nameOfCurrentTournament, numberOfPointsOfOpposingTeam - numberOfPointsOfTeamOfDesi);
+                }
+
+            }
+
+
+            input = scanner.nextLine();        //update
+        }
+
+
+        //намираме и принтираме процент спечелени мачове от всички турнири:
+        double percentageOfWonGamesFromAllTournaments = winsCount * 1.0 / totalNumberOfGamesFromAllTournaments * 100;
+        System.out.printf("%.2f%% matches win%n", percentageOfWonGamesFromAllTournaments);
+
+        //намираме и принтираме процент загубени мачове от всички турнири:
+        double percentageOfLostGamesFromAllTournaments = 100 - percentageOfWonGamesFromAllTournaments;
+        System.out.printf("%.2f%% matches lost", percentageOfLostGamesFromAllTournaments);
+    }
+}
