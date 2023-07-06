02f519191aba4b7bcb7bfef763f1fc66249b6008
Lidia Simeonova <lidia_simeonova2023@abv.bg>
2023-07-06 23:55:44 +0300

    Added solutions of one more problem with number 3

---
+++ Course 1. Programming Basics/pbExam5Date15And16June2019/P03MovieDestinationVersion2.java
@@ -0,0 +1,59 @@
+package pbExam5Date15And16June2019;
+
+import java.util.Scanner;
+
+public class P03MovieDestinationVersion2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //От конзолата се четат 4 реда:
+        double availableBudget = Double.parseDouble(scanner.nextLine());        //Бюджет на филма – реално число в диапазона [100 000.0… 2 000 000.0]
+        String destination = scanner.nextLine();      //Дестинация – текст, с възможности "Dubai", "Sofia" и "London"
+        String season = scanner.nextLine();         //Сезон – текст, с възможности "Summer" и "Winter"
+        int numberOfDays = Integer.parseInt(scanner.nextLine());      //Брой дни  – цяло число в диапазона [1… 40]
+
+        //проверяваме коя е дестинацията и кой е сезонът - и според тези данни сетваме единична цена (цена за 1 снимачен ден)
+        double priceFor1Day = 0;
+        if (destination.equals("Dubai")) {
+            if (season.equals("Winter")) {
+                priceFor1Day = 45000;
+            } else if (season.equals("Summer")) {
+                priceFor1Day = 40000;
+            }
+        } else if (destination.equals("Sofia")) {
+            if (season.equals("Winter")) {
+                priceFor1Day = 17000;
+            } else if (season.equals("Summer")) {
+                priceFor1Day = 12500;
+            }
+        } else if (destination.equals("London")) {
+            if (season.equals("Winter")) {
+                priceFor1Day = 24000;
+            } else if (season.equals("Summer")) {
+                priceFor1Day = 20250;
+            }
+        }
+
+        //намираме цената за всички снимачни дни:
+        double totalPriceForAllDays = numberOfDays * priceFor1Day;
+
+        //Ако дестинацията е Дубай – 30% отстъпка от крайната цена  -> значи, че крайната цена е 70% от цената
+        if (destination.equals("Dubai")) {
+            totalPriceForAllDays = totalPriceForAllDays * 70 * 0.01;
+        }
+
+        //Ако дестинацията е София – цената се оскъпява с 25%
+        if (destination.equals("Sofia")) {
+            totalPriceForAllDays = totalPriceForAllDays + (totalPriceForAllDays * 25 / 100);
+        }
+
+
+        //принтираме дали бюджетът е стигнал (и колко пари са останали), или не (и колко още пари са нужни):
+        if (totalPriceForAllDays <= availableBudget) {       //Ако бюджетът е достатъчен
+            System.out.printf("The budget for the movie is enough! We have %.2f leva left!", availableBudget - totalPriceForAllDays);
+        } else {        //else if (totalPriceForAllDays > availableBudget)     //Ако бюджетът НЕ е достатъчен
+            System.out.printf("The director needs %.2f leva more!", totalPriceForAllDays - availableBudget);
+        }
+
+    }
+}
